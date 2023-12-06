1fc95f18fbae1428b85368d5e240aa064dedfb07
Abner Suhett <suhettabner@gmail.com>
2023-12-06 16:20:41 -0300

    Drag And Drop list with lib using nextjs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+	DocumentContext,
+	Head,
+	Html,
+	Main,
+	NextScript,
+} from "next/document";
+import { resetServerContext } from "react-beautiful-dnd";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+interface MyDocumentProps {
+	locale: string;
 }
+export default function MyDocument({ locale }: MyDocumentProps): JSX.Element {
+	resetServerContext();
+	return (
+		<Html>
+			<Head></Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+	const initialProps = await Document.getInitialProps(ctx);
+	const locale = ctx.locale || "en";
+	return { ...initialProps, locale };
+};
